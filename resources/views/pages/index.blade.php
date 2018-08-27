67d33cacc1f7459c69d89f58ac67dc6d97f5f4ff
Muhammad Tajjiou <shutsugan@gmail.com>
2018-08-27 13:41:07 +0100

    Migrated the module, created the posts controller and a post files, added a navbar and a jumbotron to the index page, and created a new database.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,7 +7,8 @@
         <hr class="my-4">
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
         <p class="lead">
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
+            <a class="btn btn-info btn-lg" href="/register" role="button">Register</a>
         </p>
     </div>
 @endsection
